315711dbf86e9f5359a95b7435ff20ebf212c43e
Sarah Medeneche <smedenec@f2r3s2.clusters.42paris.fr>
2025-06-17 19:30:26 +0200

    small small maj

--- get_next_line.h
+++ get_next_line.h
@@ -5,13 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/17 15:04:46 by smedenec          #+#    #+#             */
-/*   Updated: 2025/06/17 17:37:23 by smedenec         ###   ########.fr       */
+/*   Created: 2025/06/17 19:19:34 by smedenec          #+#    #+#             */
+/*   Updated: 2025/06/17 19:23:20 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdio.h>
 # include <stdlib.h>
