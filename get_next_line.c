3c29fdac202fee4718835f0974688aff8d4829d1
Sarah Medeneche <smedenec@f6r6s12.clusters.42paris.fr>
2025-06-21 16:24:46 +0200

    le debut qui part pas

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 15:04:55 by smedenec          #+#    #+#             */
-/*   Updated: 2025/06/17 19:18:50 by smedenec         ###   ########.fr       */
+/*   Updated: 2025/06/21 15:36:07 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,28 @@
 
 char	*get_next_line(int fd)
 {
-	return (NULL);
-}
-// int	main(void)
-// {
-// 	int		file;
-// 	char	*line;
-// 	char	*line;
+	ssize_t	bytes;
+	size_t	lenbuff;
+	static char	*buff;
+	static	int file;
+	static	int fileopen;
 
-// 	file = open("file.txt", O_RDONLY);
-// 	if (file == -1)
-// 		return (1);
-// 	line = get_next_line(file);
-// 	printf("call gnl = %s\n", (char *)line);
-// 	return (0);
-// }
+	lenbuff = 20;
+	buff = malloc(sizeof(char) * (lenbuff + 1));
+	if (!buff)
+		return (NULL);
+
+	return (buff);
+}
+int	main(void)
+{
+	int		file;
+	char	*line;
+
+	file = open("file.txt", O_RDONLY);
+	if (file == -1)
+		return (1);
+	line = get_next_line(file);
+	printf("call gnl = %s\n", (char *)line);
+	return (0);
+}
